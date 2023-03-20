58a98a2e83a5e1a0891d98e7b301ef13ee87a275
Andriy Stryghul <andstrigul@gmail.com>
2023-03-20 15:40:25 +0200

    Injection arrayList in XML using Spring

---
+++ src/main/java/ua/stryghul/springcourse/JazzMusic.java
@@ -0,0 +1,8 @@
+package ua.stryghul.springcourse;
+
+public class JazzMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Moose the Mooche";
+    }
+}
--- src/main/java/ua/stryghul/springcourse/MusicPlayer.java
+++ src/main/java/ua/stryghul/springcourse/MusicPlayer.java
@@ -1,8 +1,10 @@
 package ua.stryghul.springcourse;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
@@ -22,17 +24,15 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer (Music music){
-        this.music = music;
-    }
-
     public MusicPlayer(){}
 
-    public void setMusic (Music music) {
-        this.music = music;
+    public void setMusic (List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing " + music.getSong());
+        for (Music music:musicList){
+            System.out.println("Playing " + music.getSong());
+        }
     }
 }
